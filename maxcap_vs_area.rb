e22acb5c4fd45c6b02810502a3f454e6d4d468ee
Edward Poore <edward.poore@gmail.com>
2011-12-02 16:46:26 -0500

    Added some functionality to check validity.

--- maxcap_vs_area.rb
+++ maxcap_vs_area.rb
@@ -17,15 +17,35 @@ area = database.getData( "area" ) # Get area values for each cell
 output_maxcaps = database.getOutputMaxCap # Get summed max caps for all cells with output pins
 
 # If a cell has no output pins, remove it from the area hash
+removeCount = 0
 area.keys.each { |key|
     unless output_maxcaps.keys.include?(key)
       area.delete(key)
+      puts "removing #{key} from the cell area hash as it has no output pins."
+      removeCount = removeCount + 1
     end
 }
 
-testplot = Plot.new( area, output_maxcaps )
-testplot.plotToScreen( :title => "Max Capacitance Driven vs. Area", :x_label => "Area", :y_label => "Max Capacitance Driven", :filename => "maxcapVsArea", :linreg => true, :outlierAnalysis => [true,1] )
-testplot.findOutliers( filename = "maxcapVsAreaOutliers.dat", k = 2)
 
+# Generate a 1D array for maxcap per unit area
+maxcapPerArea = Hash.new
+
+output_maxcaps.keys.each { |key|
+    maxcapPerArea[key] = output_maxcaps[key].to_f / area[key].to_f
+}
+
+
+
+
+scatterplot = Plot.new( area, output_maxcaps )
+scatterplot.plotToScreen( :linreg => true, :title => "Max Capacitance Driven vs. Area", :x_label => "Area", :y_label => "Max Capacitance Driven", :filename => "maxcapVsArea", :outlierAnalysis => [true,1] )
+scatterplot.findOutliers( filename = "maxcapVsAreaOutliers.dat", k = 2)
+
+histplot = Plot.new( maxcapPerArea )
+histplot.plotToScreen( :title => "Max Capacitance Driven per Unit Area", :filename => "maxcapPerUnitArea", :numBins => 50)
+
+# Information on cleaning up the data selection.
+puts "There were #{output_maxcaps.size} samples sent to the plot"
+puts "#{removeCount} cells with no output pins removed from the area hash."
 
 
