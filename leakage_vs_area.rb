6e260dcaadb960bbac441106439e5be9ab8c2e0f
Edward Poore <edward.poore@gmail.com>
2011-12-05 01:23:04 -0500

    Running Rdoc

--- leakage_vs_area.rb
+++ leakage_vs_area.rb
@@ -20,7 +20,21 @@ lkg = Hash.new
 area = database.getData( "area" )
 lkg  = database.getData( "cell_leakage_power" )
 
+leakagePerArea = Hash.new
+
+area.keys.each { |key|
+    leakagePerArea[key] = lkg[key].to_f / area[key].to_f
+}
+
+
 
 testplot = Plot.new( area, lkg )
-testplot.plotToScreen( :title => "Leakage Vs. Area", :x_label => "Area", :y_label => "Leakage", :filename => "lkgVsArea", :linreg => true, :outlierAnalysis => [true,1] )
-testplot.findOutliers( "lkgVsAreaOutliers.dat", k = 2 )
+testplot.plotToFile( :title => "Leakage Vs. Area for All Cells", :x_label => "Area in square microns", :y_label => "Leakage in microwatts", :filename => "leakageVsArea/lkgVsAreaAll", :linreg => true, :outlierAnalysis => [true,2] )
+testplot.findOutliers( "leakageVsArea/lkgVsAreaAllOutliers.dat", k = 2 )
+
+lkgUnitArea = Plot.new( area, leakagePerArea )
+lkgUnitArea.plotToFile( :title => "Leakage per Unit Area vs Area for all cells", :x_label => "Area in square microns", :y_label => "Leakage (microwatts) per square micron", :filename => "leakageVsArea/lkgUnitAreaAll", :linreg => true, :outlierAnalysis => [true,2] )
+
+hist = Plot.new( leakagePerArea )
+hist.plotToFile( :title => "Leakage per Unit Area for all cells", :x_label => "Leakage in microwatts per square micron", :filename => "leakageVsArea/lkgUnitAreaAllHist", :numBins => 50 )
+
