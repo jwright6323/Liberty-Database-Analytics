8dc31b28d7f1163b8aaf049ca2cad94b22a67dca
Edward Poore <edward.poore@gmail.com>
2011-12-14 21:50:03 -0500

    Ready for live demo

--- maxcap_vs_area.rb
+++ maxcap_vs_area.rb
@@ -10,22 +10,18 @@ require 'Plot.rb'
 $verbose = true
 
 #Declare a database on wildcat
-database = LibertyDatabase.new :mysqldb => "LibertyFileUpdate", :mysqlhost => "wildcat.ee.engr.uky.edu", :logfile => "log"
+database = LibertyDatabase.new :mysqldb => "LibertyFileUpdate",
+                               :mysqlhost => "wildcat.ee.engr.uky.edu",
+                               :logfile => "log"
 
+# Get area values for all cells
 area = Hash.new
-area = database.getData( "area" ) # Get area values for each cell
- 
-output_maxcaps = database.getOutputMaxCap # Get summed max caps for all cells with output pins
+area = database.getData( "area", :footprint => "INV" ) 
+area.merge! database.getData( "area", :footprint => "BUF" )
 
-# If a cell has no output pins, remove it from the area hash
-removeCount = 0
-area.keys.each { |key|
-    unless output_maxcaps.keys.include?(key)
-      area.delete(key)
-      #puts "removing #{key} from the cell area hash as it has no output pins."
-      removeCount = removeCount + 1
-    end
-}
+# Get max capacitance values for cells within INV and BUF
+output_maxcaps = database.getOutputMaxCap( :footprint => "INV") 
+output_maxcaps.merge!(database.getOutputMaxCap( :footprint => "BUF"))
 
 
 # Generate a 1D array for maxcap per unit area
@@ -36,18 +32,31 @@ output_maxcaps.keys.each { |key|
 }
 
 scatterplot = Plot.new( area, output_maxcaps )
-scatterplot.plotToFile(  :linreg => true, :title => "Max Capacitance Driven vs. Area for INV and BUF", :x_label => "Area in square microns", :y_label => "Max Capacitance Driven in pF", :filename => "maxcapVsArea/maxcapVsAreaINVBUF", :outlierAnalysis => [true,2] )
-scatterplot.findOutliers( filename = "maxcapVsArea/maxcapVsAreaOutliersINVBUF.dat", k = 2)
+scatterplot.generatePlot( :addOutlierLabels => 1,
+                          :logx => true,
+                          :logy => true, 
+                          :savePlot => false, 
+                          :title => "Max Capacitance Driven vs. Area for INV and BUF", 
+                          :x_label => "Area in square microns", 
+                          :y_label => "Max Capacitance Driven in pF", 
+                          :filename => "maxcapVsAreaINVBUF" )
+                            
+scatterplot.findOutliers( filename = "maxcapVsArea/maxcapVsAreaOutliersINVBUF", k = 1)
 
 unitAreaVsArea = Plot.new( area, maxcapPerArea )
-unitAreaVsArea.plotToFile( :linreg => true, :title => "Max Capacitance Driven per Unit Area vs. Area for INV and BUF", :x_label => "Area in square microns", :y_label => "Max Cap per Unit Area (pF/square micron)", :filename => "maxcapVsArea/maxcapPerAreaVsAreaINVBUF", :outlierAnalysis => [true,2] )
+unitAreaVsArea.generatePlot( :dataLabels => true, 
+                             :savePlot => false, 
+                             :title => "Max Capacitance Driven per Unit Area vs. Area for INV and BUF", 
+                             :x_label => "Area in square microns", 
+                             :y_label => "Max Cap per Unit Area (pF/square micron)", 
+                             :filename => "maxcapPerAreaVsAreaINVBUF" )
 
 
 histplot = Plot.new( maxcapPerArea )
-histplot.plotToFile( :title => "Max Capacitance Driven per Unit Area for INV and BUF", :filename => "maxcapVsArea/maxcapPerUnitAreaHistINVBUF", :numBins => 10, :x_label => "pF per square micron" )
-
-# Information on cleaning up the data selection.
-puts "There were #{output_maxcaps.size} samples sent to the plot"
-puts "#{removeCount} cells with no output pins removed from the area hash."
+histplot.generatePlot( :savePlot => false , 
+                       :title => "Max Capacitance Driven per Unit Area for INV and BUF", 
+                       :filename => "maxcapVsArea/maxcapPerUnitAreaHistINVBUF", 
+                       :numBins => 10, 
+                       :x_label => "pF per square micron" )
 
 
