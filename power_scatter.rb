f73c91a80689180bef51f3f9c41830a386a1d723
Edward Poore <edward.poore@gmail.com>
2012-02-22 01:51:15 -0500

    Check me.

--- power_scatter.rb
+++ power_scatter.rb
@@ -11,6 +11,24 @@ database = LibertyDatabase.new :mysqldb => "liberty2dbFinal",
                                :mysqlhost => "wildcat.ee.engr.uky.edu",
                                :logfile => "log",
                                :pvt => [1.0,1.32,-40]
-powerdata = database.getAllPowerData :footprint => 'INV'
 
-puts powerdata.inspect
+#show what the hash looks like:
+#powerdata = database.getAllPowerData :footprint => 'INV'
+#puts powerdata.inspect
+
+
+Hash powerdata = database.getAllPowerData
+Hash powerValues
+Hash slewValues
+
+
+powerdata.each { |pin|
+    pin.each { |when|
+        when.each { |slew|
+            powerValues[pin+when] = slew[".133417"] #power
+            slewValues[pin+when] = [slew]
+            }
+        }
+    }
+
+
